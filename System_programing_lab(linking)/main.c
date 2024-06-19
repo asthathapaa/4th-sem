744b13dc3eeef7109310e46bd093192e48375ed2
Astha Thapa <119739490+asthathapaa@users.noreply.github.com>
2024-06-19 22:54:24 +0545

    Add files via upload

---
+++ System_programing_lab(linking)/main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "tools.h"
+#define NUM1 100
+#define Num2 50
+
+int main(){
+    
+    int sum= add(NUM1,Num2);
+    printf("Sum= %d",sum);
+}
---
+++ System_programing_lab(linking)/tools.s
@@ -0,0 +1,22 @@
+	.file	"tools.c"
+	.text
+	.globl	_add
+	.def	_add;	.scl	2;	.type	32;	.endef
+_add:
+LFB0:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	movl	8(%ebp), %edx
+	movl	12(%ebp), %eax
+	addl	%edx, %eax
+	popl	%ebp
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE0:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
